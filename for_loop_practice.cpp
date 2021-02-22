12bddbb9994643076cc753f9f43274606ff253de
Bryce-rgb <71672866+Bryce-rgb@users.noreply.github.com>
2021-02-22 14:17:44 -0800

    for loop practice
    using a for loop to find the factorial of a user inputted number

---
+++ for_loop_practice.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+/*
+int main()
+{
+    for (int i = 0; i <= 10; i++)            //prints int i while i <=10, adds 1 to i each time it runs
+    {
+        std::cout << i << std::endl;         //outputs 0 -> 10
+    }
+    return 0;
+}
+*/
+
+/*
+int main()
+{
+    int factorial = 5;                                 
+    for (int i = factorial -1; i >0; i--)               
+    {
+        factorial = factorial * i;                      //5*4, 20*3, 60*2, 120*1
+
+    }
+    std::cout <<"Factorial: " << factorial << std::endl;
+
+}
+*/
+
+int main()
+{
+    int num;
+    std::cout << "Enter a integer to factorial: ";
+    std::cin >> num;
+    int factorial = num;
+        
+    for (int i = factorial - 1; i > 1; i--)
+    {
+        factorial = factorial * i;
+    }
+    std::cout << "the factorial of " << num << " is " << factorial;
+     
+    return 0;
+}
