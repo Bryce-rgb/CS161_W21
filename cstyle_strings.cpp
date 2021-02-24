b3e8fd094340855bfce4a3b04a49f05b3e62a672
Bryce-rgb <71672866+Bryce-rgb@users.noreply.github.com>
2021-02-24 12:16:27 -0800

    practice with c style string

---
+++ cstyle_strings.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<cstring>   // needed to use functions that can ready strings with spaces
+
+/*
+    Author: Bryce Davenport 02/24/2021
+    Description: Practice and testing with c style strings
+    Inputs: 
+    Outputs: 
+
+*/
+
+// c style strings are just one dimensional arrays which are terminated will the null char "\0"
+/*
+int main()
+{
+    char str[6] = "bobby";
+    std::cout << "name is: " << str;
+
+    return 0;
+}
+*/
+
+/*
+// user input of c style strings:
+int main()
+{
+    char str[100];
+    std::cout << "Enter your name: ";
+    std::cin >> str;                //only prints first name, space termiantes 
+    std::cout << str;
+
+
+
+}
+*/
+int main()
+{
+    char str[100];
+    std::cout << "Enter your name: ";
+    std::cin.getline(str,100);           //function that gets entire line including spaces, str name first then size of array in parameters  
+    std::cout << str;
+
+
+
+}
