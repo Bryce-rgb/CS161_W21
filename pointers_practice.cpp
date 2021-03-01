a6245805fb15c40b06eef439d606ba87f44ab80b
Bryce-rgb <71672866+Bryce-rgb@users.noreply.github.com>
2021-02-28 17:18:10 -0800

    pointer practice

--- pointers_practice.cpp
+++ pointers_practice.cpp
@@ -4,7 +4,9 @@
 int main()
 {
     int var = 8;
-    void* ptr = &var;                           //void because pointers dont need a type
-    std::cin.get();                             //& operator infront of variable, finds memory address, stores in ptr
-
+    void* ptr = &var;                          
+                                              //void because pointers dont need a type
+                                              //& operator infront of variable, finds memory address, stores in ptr
+    std::cin.get();                             
+    
 }
