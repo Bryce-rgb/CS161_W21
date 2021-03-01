056eda2584e3f5a40e0e7b0621aa398aadac84ac
Bryce-rgb <71672866+Bryce-rgb@users.noreply.github.com>
2021-02-28 17:17:24 -0800

    arrays testing

--- arrays_practice.cpp
+++ arrays_practice.cpp
@@ -10,9 +10,16 @@
 
 int main()
 {
-    
+    int bryce[9];
 
+    std::cout << "Element  -  Value" << std::endl;
 
+    for(int i = 0; i <=8; i++)      // int = index, i <=8 (as long as index isnt greater than size of array, i=++ (add one))
+    {
+        bryce[i] = 99;              // index [i] will be set to 99
+
+        std::cout << i << "   ------   " << bryce[i] << std::endl;
+    }
 
 
 }
